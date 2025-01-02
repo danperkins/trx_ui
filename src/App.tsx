7804e87ef5cf8e2a83e44b96b92b7c854382e961
Dan Perkins <dan@motherduck.com>
2025-01-02 15:57:30 -0500

    chore: bootstrap with Vite, Chakra UI, Prettier

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { SkeletonCircle } from "./components/ui/skeleton";
+
+function App() {
+  return (
+    <div>
+      <SkeletonCircle size="10" />
+    </div>
+  );
+}
+
+export default App;
